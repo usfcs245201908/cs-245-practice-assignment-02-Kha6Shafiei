4b053841edd77e76816c1eb593d3dacfa4ad5788
Khash Shafiei <47541695+Kha6Shafiei@users.noreply.github.com>
2019-09-02 19:09:07 -0700

    Add files via upload

---
+++ NQueens.java
@@ -0,0 +1,82 @@
+public class NQueens{
+
+	public int [][] board;
+	public int n;
+	public int row;
+	public int col;
+	
+
+	public NQueens(int n){
+		this.n = n;
+		this.board = new int [n][n];
+	}
+
+	public boolean placeNQueens(){
+
+		return placeNQueens(this.col);
+
+	}
+
+	public boolean placeNQueens(int col){
+
+		if(col == this.n ) {
+			return true;
+		}
+
+		if (this.n < 0){
+			throw new IllegalArgumentException ("N can't be smaller than zero.");
+		}
+		
+		for(int i = 0; i < this.n ; i++){
+			if(CanQueenbeThere(i,col)){
+				this.board[i][col] = 1;
+				if(placeNQueens(col + 1)){ 
+					return true;
+				}
+				this.board[i][col] = 0;
+			}
+
+		}
+
+		return false;
+}
+	
+	private boolean CanQueenbeThere(int row, int col){
+
+		int i,j;
+        for(i = 0; i < col ; i++){
+            if (board[row][i]==1) return false;
+        }
+        for( i = row, j = col;(i >= 0) && (j >=0 ); i--,j--){
+            if (board[i][j]==1) return false;
+        }
+        for(i = row, j = col ;(j >=0 ) && (i < this.n); i++,j--){
+            if (board[i][j]==1) return false;
+        }
+        return true;
+    }
+
+	public void printToConsole(){
+
+		for(int i = 0; i < n ; i++){
+			for(int j = 0; j < n; j++){
+				if(this.board[i][j] == 1){
+					System.out.print("Q ");
+				}else{
+					System.out.print("_ ");
+
+				}
+			}
+
+			System.out.println();
+		}
+	}
+
+	
+}
+	
+
+
+
+
+	
